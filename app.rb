9bd8b30edc3687a8eeb7a12f7fabb2f40db03a51
Jul Jen <juliajen.prog@gmail.com>
2018-03-28 22:21:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,16 +1,15 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
-require 'pry'
+
 
 class App < Sinatra::Base
+
   get '/' do
     erb :index
   end
 
   post '/' do
     @analyzed_text = TextAnalyzer.new(params[:user_text])
-    @analyzed_test.map do |t|
-
 
     erb :results
   end
